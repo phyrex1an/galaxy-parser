29000f1a6ab229829c3f49e5dfc619703fc9811c
Mikael Bung <phyrex1an@hotmail.com>
2010-02-28 22:32:27 +0100

    File parser

---
+++ src/Galaxy/Parser.hs
@@ -0,0 +1,73 @@
+module Galaxy.Parser where
+
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Expr
+import Galaxy.SyntaxTree
+
+import qualified Text.ParserCombinators.Parsec.Token as P
+import Text.ParserCombinators.Parsec.Language( emptyDef )
+
+lexer  = P.makeTokenParser 
+         (emptyDef
+         { 
+--commentStart  :: String
+--    Describes the start of a block comment. Use the empty string if the language doesn't support block comments. For example "/*". 
+	P.commentStart = "",
+--commentEnd :: String
+--    Describes the end of a block comment. Use the empty string if the language doesn't support block comments. For example "*/". 
+	P.commentEnd = "",
+--commentLine :: String
+--    Describes the start of a line comment. Use the empty string if the language doesn't support line comments. For example "//". 
+	P.commentLine = "//",
+--nestedComments :: Bool
+--    Set to True if the language supports nested block comments. 
+	P.nestedComments = False,
+--identStart :: CharParser st Char
+--    This parser should accept any start characters of identifiers. For example (letter <|> char "_"). 
+	P.identStart = letter,
+--identLetter :: CharParser st Char	
+--    This parser should accept any legal tail characters of identifiers. For example (alphaNum <|> char "_"). 
+	P.identLetter = alphaNum <|> oneOf "_",
+--opStart :: CharParser st Char
+--    This parser should accept any start characters of operators. For example (oneOf ":!#$%&*+./<=>?@\\^|-~") 
+	P.opStart = oneOf "+-*/=><!|&",
+--opLetter :: CharParser st Char
+--    This parser should accept any legal tail characters of operators. Note that this parser should even be defined if the language doesn't support user-defined operators, or otherwise the reservedOp parser won't work correctly. 
+	P.opLetter = oneOf "=&|",
+--reservedNames :: [String]
+--    The list of reserved identifiers. 
+	P.reservedNames = ["const", "static", "while", "break", "continue", "if", "else"],
+--reservedOpNames :: [String]
+--    The list of reserved operators.
+	P.reservedOpNames = [],
+--caseSensitive :: Bool
+--    Set to True if the language is case sensitive.
+	P.caseSensitive = True 
+	})
+
+whiteSpace= P.whiteSpace lexer
+lexeme    = P.lexeme lexer
+symbol    = P.symbol lexer
+natural   = P.natural lexer
+parens    = P.parens lexer
+braces    = P.braces lexer
+semi      = P.semi lexer
+identifier= P.identifier lexer
+reserved  = P.reserved lexer
+reservedOp= P.reservedOp lexer
+semiSep1  = P.semiSep1 lexer
+operator  = P.operator lexer
+float     = P.float lexer
+stringLiteral = P.stringLiteral lexer
+naturalOrFloat= P.naturalOrFloat lexer
+
+
+program :: String -> GenParser Char st File
+program file = lexeme . fmap (File file) $ many topDeclaration
+
+topDeclaration :: GenParser Char st TopDeclaration
+topDeclaration = undefined
+
+
+doParse :: FilePath -> String -> Either ParseError File
+doParse file input = runParser (program file) () file input
